19368be43dd420d944eca14d4492626cfd7263bc
mindotale <mindotale@gmail.com>
2022-10-01 14:27:12 +0300

    Change Presentation global usings

--- src/SiNote.Api/GlobalUsings.cs
+++ src/SiNote.Api/GlobalUsings.cs
@@ -2,4 +2,6 @@
 global using SiNote.Application;
 global using SiNote.Contracts;
 global using SiNote.Contracts.Authentication;
+global using SiNote.Contracts.Notes;
+global using FluentValidation;
 global using SiNote.Infrastructure;
